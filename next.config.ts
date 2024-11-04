35f5d7c04c4910fb5a5160a14f29b87ff64af949
michalk1924 <michalk1924@gmail.com>
2024-11-04 11:20:21 +0200

    add to config

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,21 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
+// const nextConfig: NextConfig = {
+//   /* config options here */
+// };
+
+// export default nextConfig;
+
+// next.config.js
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/home',
+        permanent: true, // Set to false if it's a temporary redirect (307)
+      },
+    ];
+  },
 };
 
-export default nextConfig;
